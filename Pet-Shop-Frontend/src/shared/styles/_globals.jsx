b56328fc7032544e23481e5d4564121e2f7ca55e
Volodymyr Berestenko <koltak2mail.ru@gmail.com>
2025-03-16 17:39:12 +0100

    feat: add global styles for consistent layout and typography across the application

---
+++ Pet-Shop-Frontend/src/shared/styles/_globals.jsx
@@ -0,0 +1,29 @@
+import { Global, css } from '@emotion/react'
+import React from 'react'
+
+export const globalStyles = (
+  <Global
+    styles={css`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+      *,
+      input,
+      textarea,
+      button {
+        font-family: 'Montserrat', sans-serif;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 130%;
+        letter-spacing: 0%;
+      }
+      body {
+        margin: 0 auto;
+        padding-bottom: 80px;
+        min-height: 100vh;
+      }
+    `}
+  />
+)
